f254c7f8eb4ae1cbbb37e9ed4d02a698f8b7e6b9
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-03-21 00:33:13 -0700

    Make increment all toolbar tab

--- app/toolbar.js
+++ app/toolbar.js
@@ -14,13 +14,21 @@ export default function Toolbar({ onIncrementAll, onIncrementGroup, exerciseList
                 Actions:
                 <ul className="flex flex-wrap gap-8 gap-y-2 font-light">
                     <li><button title="Increment weight for all exercises"
-                        onClick={() => onIncrementAll(5)}
+                        onClick={(e) => {
+                            setSelection((selection === "increment all") ? "" : "increment all");
+                            e.stopPropagation();
+                        }}
                         disabled={empty}
-                        className={empty ? disabledButtonStyles : activeButtonStyles}
+                        className={empty
+                            ? disabledButtonStyles
+                            : (activeButtonStyles
+                                + (selection === "increment all" ? "bg-neutral-800 text-gray-100 hover:bg-gray-900" : ""))}
                     >Increment All</button></li>
                     <li><button title="Increment weight by group"
-                        onClick={() =>
-                            setSelection((selection === "increment group") ? "" : "increment group")}
+                        onClick={(e) => {
+                            setSelection((selection === "increment group") ? "" : "increment group");
+                            e.stopPropagation();
+                        }}
                         disabled={empty}
                         className={empty
                             ? disabledButtonStyles
@@ -36,7 +44,41 @@ export default function Toolbar({ onIncrementAll, onIncrementGroup, exerciseList
                 && <GroupSelector
                     groups={[...new Set(exerciseList.map((exercise) => exercise.group))]}
                     onIncrement={onIncrementGroup} />}
+            {selection === "increment all"
+                && <IncrementAll
+                    onIncrement={onIncrementAll} />}
         </>
+    );
+}
+
+function IncrementAll({ onIncrement }) {
+    let [incrementAmount, setIncrementAmount] = useState(0);
+
+    const sectionStyle = "flex items-center gap-4";
+    const activeButtonStyles = "py-1.5 px-3 rounded-xl hover:bg-gray-200 active:bg-neutral-800 active:text-gray-100 ";
+    const disabledButtonStyles = "py-1.5 px-3 rounded-xl text-gray-500";
+    const buttonsDisabled = incrementAmount <= 0;
+
+    return (
+        <div className="bg-gray-300 text-gray-700 pb-6 flex flex-wrap gap-12 gap-y-4 px-12 font-semibold items-center">
+            <form onSubmit={(e) => e.preventDefault()}>
+                <label className={sectionStyle}>Amount:
+                    <input type="number" min={0} value={incrementAmount} className="w-16 p-1 font-light"
+                        onChange={(e) => setIncrementAmount(e.target.value)}
+                    />
+                </label>
+            </form>
+            <form onSubmit={(e) => e.preventDefault()}>
+                <button
+                    className={buttonsDisabled ? disabledButtonStyles : activeButtonStyles}
+                    onClick={(e) => {
+                        onIncrement(Number(incrementAmount));
+                        e.stopPropagation();
+                    }}>
+                    Apply
+                </button>
+            </form>
+        </div>
     );
 }
 
@@ -64,8 +106,8 @@ function GroupSelector({ groups, onIncrement }) {
                         <li key={uuidv4()}>
                             <button
                                 onClick={(e) => {
-                                    onIncrement(Number(incrementAmount), group)
-                                    e.stopPropagation()
+                                    onIncrement(Number(incrementAmount), group);
+                                    e.stopPropagation();
                                 }}
                                 className={buttonsDisabled ? disabledButtonStyles : activeButtonStyles}
                                 disabled={buttonsDisabled}
