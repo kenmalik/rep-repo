a056d2079ae22f5a029461b6147c31cb86a5f757
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-03-20 22:18:19 -0700

    Implement incrementation by group

--- app/workout.js
+++ app/workout.js
@@ -31,6 +31,19 @@ export default function Workout({ name, exercises, onDelete, workoutId }) {
         }));
     }
 
+    function handleIncrementGroup(amount, group) {
+        setExerciseList(exerciseList.map((exercise) => {
+            if (exercise.group === group) {
+                return {
+                    ...exercise,
+                    weight: exercise.weight + amount,
+                };
+            } else {
+                return exercise;
+            }
+        }));
+    }
+
     function handleSetWeight(id, weight) {
         setExerciseList(exerciseList.map((exercise) => {
             if (exercise.id === id) {
@@ -64,11 +77,13 @@ export default function Workout({ name, exercises, onDelete, workoutId }) {
                     </button>
                 </div>
             </form>
-            <Toolbar isEditing={isEditing} onIncrementAll={handleIncrementAll} />
+            {!isEditing && <Toolbar onIncrementAll={handleIncrementAll}
+                onIncrementGroup={handleIncrementGroup}
+                exerciseList={exerciseList} />}
             {isEditing && <AddExercise onAdd={handleAddExercise} />}
             {exerciseList.map((exercise) =>
                 <Exercise exercise={exercise} key={exercise.id} isEditable={isEditing} onDelete={handleDeleteExercise}
-                onWeightChange={handleSetWeight} />
+                    onWeightChange={handleSetWeight} />
             )}
         </div>
     );
@@ -79,11 +94,11 @@ function AddExercise({ onAdd }) {
     let [weight, setWeight] = useState(0);
     let [group, setGroup] = useState("");
 
-    const addButtonDisabled = name === "" || weight === 0 || group === "";
+    const addButtonDisabled = name === "" || weight < 0 || group === "";
 
     return (
         <form
-            className="bg-gray-300 text-gray-800 pt-2 pb-8 flex gap-6 px-12"
+            className="bg-gray-300 text-gray-800 pt-2 pb-8 flex flex-wrap gap-6 px-12"
             onSubmit={(e) => {
                 e.preventDefault();
                 onAdd({
@@ -94,7 +109,7 @@ function AddExercise({ onAdd }) {
                 setGroup("");
             }}
         >
-            <label>Add Exercise:</label>
+            <label className="font-bold">Add Exercise:</label>
             <label>
                 Name:
                 {" "}
@@ -116,14 +131,81 @@ function AddExercise({ onAdd }) {
     );
 }
 
-function Toolbar({ isEditing, onIncrementAll }) {
+function Toolbar({ onIncrementAll, onIncrementGroup, exerciseList }) {
+    let [selection, setSelection] = useState("")
+
+    const empty = exerciseList.length === 0
+    const activeButtonStyles = "py-1.5 px-4 rounded-xl hover:bg-gray-200 active:bg-neutral-800 active:text-gray-100 ";
+    const disabledButtonStyles = "py-1.5 px-4 rounded-xl text-gray-500 ";
+
     return (
-        <div className="bg-gray-300 text-gray-800 pb-6 flex gap-8 px-12 font-semibold">
-            {isEditing ? "Edit Actions:" : "Actions:"}
-            <ul className="flex gap-8 font-light">
-                <li><button title="Increment weight by group">Increment Group</button></li>
-                <li><button title="Increment weight for all exercises" onClick={() => onIncrementAll(5)}>Increment All</button></li>
-            </ul>
+        <>
+            <div className="bg-gray-300 text-gray-800 pb-6 flex gap-8 px-12 font-bold items-center">
+                Actions:
+                <ul className="flex gap-8 font-light">
+                    <li><button title="Increment weight for all exercises"
+                        onClick={() => onIncrementAll(5)}
+                        disabled={empty}
+                        className={empty ? disabledButtonStyles : activeButtonStyles}
+                    >Increment All</button></li>
+                    <li><button title="Increment weight by group"
+                        onClick={() =>
+                            setSelection((selection === "increment group") ? "" : "increment group")}
+                        disabled={empty}
+                        className={empty
+                            ? disabledButtonStyles
+                            : (activeButtonStyles
+                                + (selection === "increment group" ? "bg-neutral-800 text-gray-100 hover:bg-gray-900" : ""))
+                        }
+                    >
+                        Increment Group
+                    </button></li>
+                </ul>
+            </div>
+            {selection === "increment group"
+                && <GroupSelector
+                    groups={[...new Set(exerciseList.map((exercise) => exercise.group))]}
+                    onIncrement={onIncrementGroup} />}
+        </>
+    );
+}
+
+function GroupSelector({ groups, onIncrement }) {
+    let [incrementAmount, setIncrementAmount] = useState(0);
+
+    const sectionStyle = "flex items-center gap-4";
+    const activeButtonStyles = "py-1.5 px-3 rounded-xl hover:bg-gray-200 active:bg-neutral-800 active:text-gray-100 ";
+    const disabledButtonStyles = "py-1.5 px-3 rounded-xl text-gray-500";
+    const buttonsDisabled = incrementAmount <= 0;
+
+    return (
+        <div className="bg-gray-300 text-gray-700 pb-6 flex gap-12 px-12 font-semibold items-center">
+            <form onSubmit={(e) => e.preventDefault()}>
+                <label className={sectionStyle}>Amount:
+                    <input type="number" min={0} value={incrementAmount} className="w-16 p-1 font-light"
+                        onChange={(e) => setIncrementAmount(e.target.value)}
+                    />
+                </label>
+            </form>
+            <form onSubmit={(e) => e.preventDefault()} className={sectionStyle}>
+                <label>Select Group to Increment:</label>
+                <ul className="flex gap-4 font-light">
+                    {groups.map((group) =>
+                        <li key={uuidv4()}>
+                            <button
+                                onClick={(e) => {
+                                    onIncrement(Number(incrementAmount), group)
+                                    e.stopPropagation()
+                                }}
+                                className={buttonsDisabled ? disabledButtonStyles : activeButtonStyles}
+                                disabled={buttonsDisabled}
+                            >
+                                {group}
+                            </button>
+                        </li>)
+                    }
+                </ul>
+            </form>
         </div>
     );
 }
