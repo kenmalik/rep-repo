16465e1ec9f4db226b4ca56d704fe135fff7908a
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-03-25 19:02:23 -0700

    Add login form to splash and restyle

--- app/page.js
+++ app/page.js
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export default function Splash() {
   return (
-    <main className="bg-white text-gray-800 flex justify-center sm:justify-normal h-full">
-      <div className="items-stretch flex flex-col sm:flex-row sm:ml-32 md:ml-48 lg:ml-64 mt-16 sm:self-center">
-        <div className="text-center p-8">
+    <main className="bg-gray-200 text-gray-800 flex justify-center md:justify-normal h-full">
+      <div className="items-stretch flex flex-col mt-16 md:mt-0 md:flex-row md:ml-24 lg:ml-48 md:self-center">
+        <div className="text-center md:text-right p-8 md:my-auto">
           <p>Welcome to</p>
           <div className="font-bold italic text-4xl">REP REPO</div>
         </div>
-        <div className="border-r-gray-500 border-b-2 sm:border-r-2"></div>
+        <div className="border-r-gray-500 border-b-2 md:border-r-2"></div>
         <Buttons />
       </div>
     </main>
@@ -16,10 +16,34 @@ export default function Splash() {
 }
 
 function Buttons() {
+  const inputStyles = "bg-gray-100 block";
+  const buttonStyles =
+    "px-6 py-1 text-white bg-green-600 hover:bg-green-500 mr-4 rounded-lg";
+
   return (
     <div className="p-8">
-      <div>Login</div>
-      <Link href="your-repo">Go</Link>
+      <form className="mb-4">
+        <label className="block mb-4">
+          Username
+          <input
+            id="username"
+            type="text"
+            className={inputStyles}
+            autoComplete="on"
+          />
+        </label>
+        <label className="block mb-8">
+          Password
+          <input id="password" type="password" className={inputStyles} />
+        </label>
+        <div className="mb-12">
+          <button className={buttonStyles}>Log in</button>
+          <button className={buttonStyles}>Sign up</button>
+        </div>
+      </form>
+      <Link href="your-repo" className="hover:underline">
+        Continue as guest
+      </Link>
     </div>
   );
 }
