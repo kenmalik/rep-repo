b5924e9bc7df6cc861bcc9be1aa602b7cde281ec
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-06-26 17:15:36 -0700

    fix: add logout button back for mobile

--- app/your-repo/header.js
+++ app/your-repo/header.js
@@ -16,14 +16,9 @@ export default function Header() {
   }
 
   return (
-    <div
-      className="flex items-center justify-center bg-gray-200 px-24 py-4 text-gray-800
-        sm:justify-between sm:py-6"
-    >
+    <div className="flex items-center justify-between bg-gray-200 px-6 py-4 text-gray-800 sm:flex-row sm:py-6">
       <div className="text-3xl font-extrabold italic sm:text-4xl">REP REPO</div>
-      <div className="hidden sm:block">
-        <button onClick={() => handleSignOut()}>Log out</button>
-      </div>
+      <button onClick={() => handleSignOut()}>Log out</button>
     </div>
   );
 }
